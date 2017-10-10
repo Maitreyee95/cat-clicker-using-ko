fabb667a5ed5721d6b761afd200210d0d30421e4
Maitreyee95 <maitreyee95@gmail.com>
2017-10-11 01:36:12 +0530

    use 'with'

--- js/app.js
+++ js/app.js
@@ -16,9 +16,15 @@ var Cat=function(){
 
 };
 var ViewModel= function(){
+	// var self= this; //self refers to the binding context of ViewModel
 	this.currentCat= ko.observable( new Cat());
 	this.incrementCounter=function(){
-		this.currentCat().clickCount(this.currentCat().clickCount()+1);
+		// this.currentCat().clickCount(this.currentCat().clickCount()+1);//without 'with' in Dom,while clicking the image,we were in the binding context of ViewModel()
+		this.clickCount(this.clickCount()+1); //with the 'with' binding in DOM
+											  //we are already in the binding context of currentCat(). so we cant mention it again.
+											  //here 'this' refers to the binding context of currentCat()
+	//--another solution
+	 // self.currentCat().clickCount(self.currentCat().clickCount()+1); //we are still using 'with' but here,we are using binding context pf ViewModel.this too works
 	};
 
 };
