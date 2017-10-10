e9c581577f16bcebf7597d844bf9c984545f63cb
Maitreyee95 <maitreyee95@gmail.com>
2017-10-11 02:51:19 +0530

    add more cats and change view of cat when you click on different cats in the list

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
-var Cat=function(){
-	this.clickCount=ko.observable(0);
-	this.name=ko.observable('Ben');
-	this.imgSrc=ko.observable('img/9648464288_2516b35537_z.jpg');
+var initialCats=[
+{
+	clickCount:0,
+	name:"Tabby",
+	imgSrc: "img/cat1.jpg",
+	nickname:['Tab']
+},
+{
+	clickCount:0,
+	name:"Tommy",
+	imgSrc: "img/cat2.jpg",
+	nickname:['Tom']
+},
+{
+	clickCount:0,
+	name:"Benny",
+	imgSrc: "img/cat3.jpg",
+	nickname:['Ben']
+},
+{
+	clickCount:0,
+	name:"Bobby",
+	imgSrc: "img/cat4.jpg",
+	nickname:['Bob']
+},
+{
+	clickCount:0,
+	name:"Bonny",
+	imgSrc: "img/cat5.jpg",
+	nickname:['Bon']
+}
+];
+
+
+var Cat=function(data){
+	this.clickCount=ko.observable(data.clickCount);
+	this.name=ko.observable(data.name);
+	this.imgSrc=ko.observable(data.imgSrc);
 	this.catLevel=ko.computed(function(){
 		if(this.clickCount()<10){
 			return "newborn";
@@ -12,20 +46,33 @@ var Cat=function(){
 			return "ninja";
 		}
 	},this);
-	this.nickname= ko.observableArray(["benny","tommy"]);
+	this.nickname= ko.observableArray(data.nickname);
 
 };
+
+
 var ViewModel= function(){
-	// var self= this; //self refers to the binding context of ViewModel
-	this.currentCat= ko.observable( new Cat());
+	var self= this; //self refers to the binding context of ViewModel
+
+	this.catList= ko.observableArray([]);//this=viewmodel
+	initialCats.forEach(function(catItem){
+		self.catList.push(new Cat(catItem));
+
+	});
+
+	this.currentCat= ko.observable(this.catList()[0]); //this=ViewModel
 	this.incrementCounter=function(){
 		// this.currentCat().clickCount(this.currentCat().clickCount()+1);//without 'with' in Dom,while clicking the image,we were in the binding context of ViewModel()
-		this.clickCount(this.clickCount()+1); //with the 'with' binding in DOM
+		// this.clickCount(this.clickCount()+1); //with the 'with' binding in DOM
 											  //we are already in the binding context of currentCat(). so we cant mention it again.
 											  //here 'this' refers to the binding context of currentCat()
-	//--another solution
-	 // self.currentCat().clickCount(self.currentCat().clickCount()+1); //we are still using 'with' but here,we are using binding context pf ViewModel.this too works
+		//--another solution
+		 self.currentCat().clickCount(self.currentCat().clickCount()+1); //we are still using 'with' but here,we are using binding context pf ViewModel.this too works
 	};
+
+	this.setCat=function(clickedCat){ //passing the model we clicked on
+		self.currentCat(clickedCat);
+	}
 
 };
 
